5b400e232bcabd8808ca2e08327e048be34fb52f
Vitvor <112934325+Vitvor-ua@users.noreply.github.com>
2025-01-03 20:50:35 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lab3","l":"Image"},{"p":"lab3","l":"RealImage"},{"p":"lab3","l":"RestrictedImageDecorator"}];updateSearchResults();
